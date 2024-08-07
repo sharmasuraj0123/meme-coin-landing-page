b1fdb70c1fa77514de4dd6931335b1902fd49079
Suraj <sharmasuraj0123@gmail.com>
2024-08-07 21:37:45 +0530

    fix Errors

--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,7 @@ export default function Home() {
   const iconMap = {
     Rocket: Rocket,
     Coins: Coins,
-    Users: Users
+    Users: Users                                                   
   };
 
   return (
